576176f2ef499047ec25d83efd9b18ca943f50d1
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2024-05-29 08:18:37 +0000

    Ajout du css

--- vue/inc/accueil.php
+++ vue/inc/accueil.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./../../index.css">
+    <link rel="stylesheet" href="index.css">
     <title>Actualités MGLSI</title>
+    <style>
+        
+    </style>
 </head>
 <body>
     <?php
